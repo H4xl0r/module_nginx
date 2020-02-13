5db638bb6e304fd2871239e3db28f982ebfdd07a
H4xl0r <31049432+H4xl0r@users.noreply.github.com>
2020-02-13 17:53:48 +0100

    Update _info_.php

--- _info_.php
+++ _info_.php
@@ -1,14 +1,18 @@
 <?
 $mod_name="nginx";
-$mod_version="1.1";
+$mod_version="2.b";
 $mod_path="/usr/share/fruitywifi/www/modules/$mod_name";
 $mod_logs="$log_path/$mod_name.log"; 
 $mod_logs_history="$mod_path/includes/logs/";
-$mod_logs_panel="disabled";
+$mod_logs_panel="enabled";
 $mod_panel="show";
 $mod_type="service";
 $mod_isup="ps auxww | grep -E 'nginx.+/modules/nginx/.+nginx.conf' | grep -v -e grep";
+$mod_coisup="sudo /sbin/iptables -t nat -L|grep -iEe 'mark match ! 0x63'";
+$mod_coisupopen = "grep -iEe '^iptables -t nat -A PREROUTING' /usr/share/fruitywifi/www/modules/ap/includes/dnsmasq-dhcp-script.sh";
+$mod_coconf= "$mod_path/includes/vhost-captive.conf";
 $mod_alias="Nginx";
+$mod_co="Captive";
 
 # OPTIONS
 $mod_nginx_fpm = "php5";
@@ -16,6 +20,7 @@ $mod_nginx_fpm = "php5";
 # EXEC
 $bin_sudo = "/usr/bin/sudo";
 $bin_nginx = "/usr/sbin/nginx";
+$bin_captive = "/var/www/captive";
 $bin_awk = "/usr/bin/awk";
 $bin_grep = "/bin/grep";
 $bin_sed = "/bin/sed";
